0b578ea9567f3481a145fd2e7a22029e2d032933
xiaofant <xiaofant@7c85d151-7abd-4ef8-aa04-6f1aa9a6239e>
2013-02-06 20:57:32 +0000

    change controllers to support admin UI

---
+++ grails-app/controllers/metridoc/rid/RidDepartmentalAffiliationController.groovy
@@ -0,0 +1,105 @@
+package metridoc.rid
+
+import org.springframework.dao.DataIntegrityViolationException
+
+class RidDepartmentalAffiliationController {
+
+    static allowedMethods = [save: "POST", update: "POST", delete: "POST"]
+
+    def index() {
+        redirect(action: "list", params: params)
+    }
+
+    def list(Integer max) {
+        params.max = Math.min(max ?: 10, 100)
+        [ridDepartmentalAffiliationInstanceList: RidDepartmentalAffiliation.list(params), ridDepartmentalAffiliationInstanceTotal: RidDepartmentalAffiliation.count()]
+    }
+
+    def create() {
+        [ridDepartmentalAffiliationInstance: new RidDepartmentalAffiliation(params)]
+    }
+
+    def save() {
+        def ridDepartmentalAffiliationInstance = new RidDepartmentalAffiliation(params)
+        if (!ridDepartmentalAffiliationInstance.save(flush: true)) {
+            render(view: "create", model: [ridDepartmentalAffiliationInstance: ridDepartmentalAffiliationInstance])
+            return
+        }
+
+        flash.message = message(code: 'default.created.message', args: [message(code: 'ridDepartmentalAffiliation.label', default: 'RidDepartmentalAffiliation'), ridDepartmentalAffiliationInstance.id])
+        //redirect(action: "show", id: ridDepartmentalAffiliationInstance.id)
+        redirect(action: "list")
+    }
+
+    def show(Long id) {
+        def ridDepartmentalAffiliationInstance = RidDepartmentalAffiliation.get(id)
+        if (!ridDepartmentalAffiliationInstance) {
+            flash.message = message(code: 'default.not.found.message', args: [message(code: 'ridDepartmentalAffiliation.label', default: 'RidDepartmentalAffiliation'), id])
+            redirect(action: "list")
+            return
+        }
+
+        [ridDepartmentalAffiliationInstance: ridDepartmentalAffiliationInstance]
+    }
+
+    def edit(Long id) {
+        def ridDepartmentalAffiliationInstance = RidDepartmentalAffiliation.get(id)
+        if (!ridDepartmentalAffiliationInstance) {
+            flash.message = message(code: 'default.not.found.message', args: [message(code: 'ridDepartmentalAffiliation.label', default: 'RidDepartmentalAffiliation'), id])
+            redirect(action: "list")
+            return
+        }
+
+        [ridDepartmentalAffiliationInstance: ridDepartmentalAffiliationInstance]
+    }
+
+    def update(Long id, Long version) {
+        def ridDepartmentalAffiliationInstance = RidDepartmentalAffiliation.get(id)
+        if (!ridDepartmentalAffiliationInstance) {
+            flash.message = message(code: 'default.not.found.message', args: [message(code: 'ridDepartmentalAffiliation.label', default: 'RidDepartmentalAffiliation'), id])
+            redirect(action: "list")
+            return
+        }
+
+        if (version != null) {
+            if (ridDepartmentalAffiliationInstance.version > version) {
+                ridDepartmentalAffiliationInstance.errors.rejectValue("version", "default.optimistic.locking.failure",
+                        [message(code: 'ridDepartmentalAffiliation.label', default: 'RidDepartmentalAffiliation')] as Object[],
+                        "Another user has updated this RidDepartmentalAffiliation while you were editing")
+                render(view: "edit", model: [ridDepartmentalAffiliationInstance: ridDepartmentalAffiliationInstance])
+                return
+            }
+        }
+
+        ridDepartmentalAffiliationInstance.properties = params
+
+        if (!ridDepartmentalAffiliationInstance.save(flush: true)) {
+            render(view: "edit", model: [ridDepartmentalAffiliationInstance: ridDepartmentalAffiliationInstance])
+            return
+        }
+
+        flash.message = message(code: 'default.updated.message', args: [message(code: 'ridDepartmentalAffiliation.label', default: 'RidDepartmentalAffiliation'), ridDepartmentalAffiliationInstance.id])
+        //redirect(action: "show", id: ridDepartmentalAffiliationInstance.id)
+        redirect(action: "list")
+    }
+
+    def delete(Long id) {
+        def ridDepartmentalAffiliationInstance = RidDepartmentalAffiliation.get(id)
+        if (!ridDepartmentalAffiliationInstance) {
+            flash.message = message(code: 'default.not.found.message', args: [message(code: 'ridDepartmentalAffiliation.label', default: 'RidDepartmentalAffiliation'), id])
+            redirect(action: "list")
+            return
+        }
+
+        try {
+            ridDepartmentalAffiliationInstance.delete(flush: true)
+            flash.message = message(code: 'default.deleted.message', args: [message(code: 'ridDepartmentalAffiliation.label', default: 'RidDepartmentalAffiliation'), id])
+            redirect(action: "list")
+        }
+        catch (DataIntegrityViolationException e) {
+            flash.message = message(code: 'default.not.deleted.message', args: [message(code: 'ridDepartmentalAffiliation.label', default: 'RidDepartmentalAffiliation'), id])
+            //redirect(action: "show", id: id)
+            redirect(action: "list")
+        }
+    }
+}
---
+++ grails-app/controllers/metridoc/rid/RidServiceProvidedController.groovy
@@ -0,0 +1,105 @@
+package metridoc.rid
+
+import org.springframework.dao.DataIntegrityViolationException
+
+class RidServiceProvidedController {
+
+    static allowedMethods = [save: "POST", update: "POST", delete: "POST"]
+
+    def index() {
+        redirect(action: "list", params: params)
+    }
+
+    def list(Integer max) {
+        params.max = Math.min(max ?: 10, 100)
+        [ridServiceProvidedInstanceList: RidServiceProvided.list(params), ridServiceProvidedInstanceTotal: RidServiceProvided.count()]
+    }
+
+    def create() {
+        [ridServiceProvidedInstance: new RidServiceProvided(params)]
+    }
+
+    def save() {
+        def ridServiceProvidedInstance = new RidServiceProvided(params)
+        if (!ridServiceProvidedInstance.save(flush: true)) {
+            render(view: "create", model: [ridServiceProvidedInstance: ridServiceProvidedInstance])
+            return
+        }
+
+        flash.message = message(code: 'default.created.message', args: [message(code: 'ridServiceProvided.label', default: 'RidServiceProvided'), ridServiceProvidedInstance.id])
+        //redirect(action: "show", id: ridServiceProvidedInstance.id)
+        redirect(action: "list")
+    }
+
+    def show(Long id) {
+        def ridServiceProvidedInstance = RidServiceProvided.get(id)
+        if (!ridServiceProvidedInstance) {
+            flash.message = message(code: 'default.not.found.message', args: [message(code: 'ridServiceProvided.label', default: 'RidServiceProvided'), id])
+            redirect(action: "list")
+            return
+        }
+
+        [ridServiceProvidedInstance: ridServiceProvidedInstance]
+    }
+
+    def edit(Long id) {
+        def ridServiceProvidedInstance = RidServiceProvided.get(id)
+        if (!ridServiceProvidedInstance) {
+            flash.message = message(code: 'default.not.found.message', args: [message(code: 'ridServiceProvided.label', default: 'RidServiceProvided'), id])
+            redirect(action: "list")
+            return
+        }
+
+        [ridServiceProvidedInstance: ridServiceProvidedInstance]
+    }
+
+    def update(Long id, Long version) {
+        def ridServiceProvidedInstance = RidServiceProvided.get(id)
+        if (!ridServiceProvidedInstance) {
+            flash.message = message(code: 'default.not.found.message', args: [message(code: 'ridServiceProvided.label', default: 'RidServiceProvided'), id])
+            redirect(action: "list")
+            return
+        }
+
+        if (version != null) {
+            if (ridServiceProvidedInstance.version > version) {
+                ridServiceProvidedInstance.errors.rejectValue("version", "default.optimistic.locking.failure",
+                        [message(code: 'ridServiceProvided.label', default: 'RidServiceProvided')] as Object[],
+                        "Another user has updated this RidServiceProvided while you were editing")
+                render(view: "edit", model: [ridServiceProvidedInstance: ridServiceProvidedInstance])
+                return
+            }
+        }
+
+        ridServiceProvidedInstance.properties = params
+
+        if (!ridServiceProvidedInstance.save(flush: true)) {
+            render(view: "edit", model: [ridServiceProvidedInstance: ridServiceProvidedInstance])
+            return
+        }
+
+        flash.message = message(code: 'default.updated.message', args: [message(code: 'ridServiceProvided.label', default: 'RidServiceProvided'), ridServiceProvidedInstance.id])
+        //redirect(action: "show", id: ridServiceProvidedInstance.id)
+        redirect(action: "list")
+    }
+
+    def delete(Long id) {
+        def ridServiceProvidedInstance = RidServiceProvided.get(id)
+        if (!ridServiceProvidedInstance) {
+            flash.message = message(code: 'default.not.found.message', args: [message(code: 'ridServiceProvided.label', default: 'RidServiceProvided'), id])
+            redirect(action: "list")
+            return
+        }
+
+        try {
+            ridServiceProvidedInstance.delete(flush: true)
+            flash.message = message(code: 'default.deleted.message', args: [message(code: 'ridServiceProvided.label', default: 'RidServiceProvided'), id])
+            redirect(action: "list")
+        }
+        catch (DataIntegrityViolationException e) {
+            flash.message = message(code: 'default.not.deleted.message', args: [message(code: 'ridServiceProvided.label', default: 'RidServiceProvided'), id])
+            //redirect(action: "show", id: id)
+            redirect(action: "list")
+        }
+    }
+}
